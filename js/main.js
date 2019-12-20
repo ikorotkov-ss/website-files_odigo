6c2cba270f15e6e44e196c372ab2a6c27d72fa87
Igor <korotkovigorby@gmail.com>
2019-12-20 16:07:44 +0300

    Медиа запрос для 960px

--- js/main.js
+++ js/main.js
@@ -14,7 +14,11 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav-active');
     })
 }())
